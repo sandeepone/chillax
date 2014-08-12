437729761874e91e3773b9a96cd027aa2e365de7
Didip Kerabat <didipk@gmail.com>
2014-08-11 19:28:41 -0700

    Added test for split docker ports

--- libstring/libstring_test.go
+++ libstring/libstring_test.go
@@ -20,11 +20,36 @@ func TestNormalizeLocalhost(t *testing.T) {
 }
 
 func TestStripProtocol(t *testing.T) {
-    hostname, _ := os.Hostname()
-
-    if StripProtocol("tcp://localhost:2375") != (hostname + ":2375") {
+    if StripProtocol("tcp://localhost:2375") != "localhost:2375" {
         t.Errorf("Failed to strip protocol: %v", StripProtocol("tcp://localhost:2375"))
     }
 }
 
+func TestSplitDockerPorts(t *testing.T) {
+    ports := "127.0.0.1:99999:80/tcp"
+    hostIp, hostPort, containerPort := SplitDockerPorts(ports)
+
+    if hostIp != "127.0.0.1" {
+        t.Errorf("Failed to split docker ports. hostIp: %v, hostPort: %v, containerPort: %v", hostIp, hostPort, containerPort)
+    }
+    if hostPort != "99999" {
+        t.Errorf("Failed to split docker ports. hostIp: %v, hostPort: %v, containerPort: %v", hostIp, hostPort, containerPort)
+    }
+    if containerPort != "80/tcp" {
+        t.Errorf("Failed to split docker ports. hostIp: %v, hostPort: %v, containerPort: %v", hostIp, hostPort, containerPort)
+    }
+
+    ports = "99999:80/tcp"
+    hostIp, hostPort, containerPort = SplitDockerPorts(ports)
+
+    if hostIp != "" {
+        t.Errorf("Failed to split docker ports. hostIp: %v, hostPort: %v, containerPort: %v", hostIp, hostPort, containerPort)
+    }
+    if hostPort != "99999" {
+        t.Errorf("Failed to split docker ports. hostIp: %v, hostPort: %v, containerPort: %v", hostIp, hostPort, containerPort)
+    }
+    if containerPort != "80/tcp" {
+        t.Errorf("Failed to split docker ports. hostIp: %v, hostPort: %v, containerPort: %v", hostIp, hostPort, containerPort)
+    }
+}
 
