fa533fa380e02939a405e8246c1d8849d5ffd6ca
Didip Kerabat <didipk@gmail.com>
2014-08-16 15:31:23 -0700

    Round Robin strategy.

---
+++ proxy/selectors/round_robin.go
@@ -0,0 +1,67 @@
+package selectors
+
+import (
+    "container/ring"
+    "sync"
+)
+
+type RoundRobin struct {
+    r   *ring.Ring
+    l   sync.RWMutex
+}
+
+func NewRoundRobin(strs []string) *RoundRobin {
+    r := ring.New(len(strs))
+    for _, s := range strs {
+        r.Value = s
+        r = r.Next()
+    }
+    return &RoundRobin{r: r}
+}
+
+func (rr *RoundRobin) Len() int {
+    rr.l.RLock()
+    defer rr.l.RUnlock()
+    return rr.r.Len()
+}
+
+func (rr *RoundRobin) Choose() string {
+    rr.l.Lock()
+    defer rr.l.Unlock()
+    if rr.r == nil {
+        return ""
+    }
+    n := rr.r.Value.(string)
+    rr.r = rr.r.Next()
+    return n
+}
+
+func (rr *RoundRobin) Add(s string) {
+    rr.l.Lock()
+    defer rr.l.Unlock()
+    nr := &ring.Ring{Value: s}
+    if rr.r == nil {
+        rr.r = nr
+    } else {
+        rr.r = rr.r.Link(nr).Next()
+    }
+}
+
+func (rr *RoundRobin) Remove(s string) {
+    rr.l.Lock()
+    defer rr.l.Unlock()
+    r := rr.r
+    if rr.r.Len() == 1 {
+        rr.r = ring.New(0)
+        return
+    }
+
+    for i := rr.r.Len(); i > 0; i-- {
+        r = r.Next()
+        ba := r.Value.(string)
+        if s == ba {
+            rr.r = r.Unlink(1)
+            return
+        }
+    }
+}
