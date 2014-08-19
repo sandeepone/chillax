6f3a8bfba4c38900973836f90ec49e0f56c8d8f1
Didip Kerabat <didipk@gmail.com>
2014-08-18 23:48:01 -0700

    First commit on proxy server.

---
+++ web/server/server.go
@@ -0,0 +1,94 @@
+package server
+
+import (
+    "os"
+    "path"
+    "path/filepath"
+    "bufio"
+    "io/ioutil"
+
+    "github.com/zenazn/goji"
+    "github.com/didip/chillax/libenv"
+    chillax_proxy_handler "github.com/didip/chillax/proxy/handler"
+)
+
+func NewHttpServer() (*HttpServer, error) {
+    server := &HttpServer{}
+
+    err := server.LoadProxyHandlersFromConfig()
+    if err != nil { return server, err }
+
+    return server, err
+}
+
+type HttpServer struct {
+    ProxyHandlers []*chillax_proxy_handler.ProxyHandler
+}
+
+func (h *HttpServer) LoadProxyHandlersFromConfig() error {
+    defaultProxyBackendsDir := libenv.EnvWithDefault("DEFAULT_PROXY_BACKENDS_DIR", "")
+
+    if defaultProxyBackendsDir != "" {
+        files, err := filepath.Glob(path.Join(defaultProxyBackendsDir, "*.toml"))
+        if err != nil { return err }
+
+        h.ProxyHandlers = make([]*chillax_proxy_handler.ProxyHandler, len(files))
+
+        for i, file := range files {
+            fullFilename    := path.Join(defaultProxyBackendsDir, file)
+            fileHandle, err := os.Open(fullFilename)
+
+            if err != nil { return err }
+
+            bufReader       := bufio.NewReader(fileHandle)
+            definition, err := ioutil.ReadAll(bufReader)
+
+            if err != nil { return err }
+
+            h.ProxyHandlers[i] = chillax_proxy_handler.NewProxyHandler(definition)
+        }
+    }
+    return nil
+}
+
+func (h *HttpServer) ReloadProxyHandlers() {
+    h.ProxyHandlers = chillax_proxy_handler.NewProxyHandlers()
+}
+
+func (h *HttpServer) CreateProxyBackends() []error {
+    errors := make([]error, 0)
+
+    for _, handler := range h.ProxyHandlers {
+        err := handler.CreateBackends()
+        if err != nil { errors = append(errors, err) }
+    }
+
+    return errors
+}
+
+func (h *HttpServer) StartProxyBackends() []error {
+    errors := make([]error, 0)
+
+    for _, handler := range h.ProxyHandlers {
+        errs := handler.StartBackends()
+        if errs != nil { errors = append(errors, errs...) }
+    }
+
+    return errors
+}
+
+func (h *HttpServer) StopProxyBackends() []error {
+    errors := make([]error, 0)
+
+    for _, handler := range h.ProxyHandlers {
+        errs := handler.StopBackends()
+        if errs != nil { errors = append(errors, errs...) }
+    }
+
+    return errors
+}
+
+func (h *HttpServer) Serve() {
+    goji.Serve()
+}
+
---
+++ web/server/server_test.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+    "os"
+    "testing"
+    "github.com/didip/chillax/libtime"
+)
+
+
+func NewServerForTest(t *testing.T) *HttpServer {
+    os.Setenv("DEFAULT_PROXY_BACKENDS_DIR", "./example-default-backend-dir")
+    server, err := NewHttpServer()
+
+    if err != nil {
+        t.Errorf("Failed to create HTTP server cleanly. Error: %v", err)
+    }
+
+    return server
+}
+
+func TestServerStartStopBackends(t *testing.T) {
+    server := NewServerForTest(t)
+
+    errors := server.CreateProxyBackends()
+    for _, err := range errors {
+        if err != nil {
+            t.Errorf("Failed to create backends. Error: %v", err)
+        }
+    }
+
+    errors = server.StartProxyBackends()
+    for _, err := range errors {
+        if err != nil {
+            t.Errorf("Failed to start backends. Error: %v", err)
+        }
+    }
+
+    libtime.SleepString("1s")
+
+    errors = server.StopProxyBackends()
+    for _, err := range errors {
+        if err != nil {
+            t.Errorf("Failed to stop backends. Error: %v", err)
+        }
+    }
+}
