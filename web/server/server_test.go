2645a086f28521a467709f4d7ea575f27af6cc39
Didip Kerabat <didipk@gmail.com>
2014-09-01 09:30:35 -0700

    Should be able to create server without CONFIG_PATH. Added test for it.

---
+++ web/server/server_test.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+    "testing"
+)
+
+func TestNewServerWithoutConfigPath(t *testing.T) {
+    server, err := NewServer()
+
+    if err != nil {
+        t.Errorf("Should be able to create server without CONFIG_PATH. Error: %v", err)
+    }
+    if server.AdminProxiesPath == "" {
+        t.Errorf("server.AdminProxiesPath should not be empty")
+    }
+    if server.AdminStaticPath == "" {
+        t.Errorf("server.AdminStaticPath should not be empty")
+    }
+}
