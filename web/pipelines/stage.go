297dd7599438fb03a9146bb37e86281d1fdd8593
Didip Kerabat <didipk@gmail.com>
2014-09-04 00:16:48 -0700

    Failed run should set LastFailUnixNano.

--- web/pipelines/stage.go
+++ web/pipelines/stage.go
@@ -7,6 +7,7 @@ import (
 
 type Stage struct {
     LastSuccessUnixNano int64
+    LastFailUnixNano    int64
 }
 
 func (s *Stage) NewRequest(uri string) *goreq.Request {
@@ -17,11 +18,12 @@ func (s *Stage) NewRequest(uri string) *goreq.Request {
     }
 }
 
-func (s *Stage) Run(req *goreq.Request) {
+func (s *Stage) Run(req *goreq.Request) error {
     _, err := req.Do()
     if err == nil {
         s.LastSuccessUnixNano = time.Now().UnixNano()
     } else {
-
+        s.LastFailUnixNano = time.Now().UnixNano()
     }
+    return err
 }
--- web/pipelines/stage_test.go
+++ web/pipelines/stage_test.go
@@ -20,3 +20,19 @@ func TestStageNewRequest(t *testing.T) {
         t.Error("Default timeout should be 1 second.")
     }
 }
+
+func TestStageRunBadRequest(t *testing.T) {
+    stage := &Stage{}
+
+    req := stage.NewRequest("http://localhost:3000")
+    req.Timeout = 1 * time.Millisecond
+
+    err := stage.Run(req)
+
+    if err == nil {
+        t.Error("Request should fail.")
+    }
+    if stage.LastFailUnixNano < 0 {
+        t.Error("Request should fail and set req.LastFailUnixNano.")
+    }
+}
