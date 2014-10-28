7b14edee6fe7c6153091b9952b1b590b7968a7a1
Didip Kerabat <didipk@gmail.com>
2014-10-27 22:34:39 -0700

    Able to show individual proxy data on /admin/proxies/{name}

--- web/templates/admin/proxy.go
+++ web/templates/admin/proxy.go
@@ -26,40 +26,73 @@ func (p *AdminProxy) BaseString() string {
 func (p *AdminProxy) String() string {
 	return `
 <div class="row">
-    <div class="large-12 columns">
-        <h2>Proxy {{ .ProxyBackend.Domain }}{{ .ProxyBackend.Path }}</h2>
-    </div>
+	<div class="large-12 columns">
+		<h2>Proxy {{ .ProxyBackend.Domain }}{{ .ProxyBackend.Path }}</h2>
+	</div>
 </div>
 
 <div class="row">
-    <div class="large-12 columns">
-        <table class="full-width">
-            <thead>
-                <tr>
-                    <th>Domain</th>
-                    <th>Path</th>
-                    <th>Command</th>
-                    <th>Environment Variables</th>
-                    <th width="50">Processes</th>
-                    <th width="50">Process Ping Interval</th>
-                    <th width="50">Process Ping Delay</th>
-                </tr>
-            </thead>
+	<div class="large-12 columns">
+		<table class="full-width">
+			<thead>
+				<tr>
+					<th>Domain</th>
+					<th>Path</th>
+					<th>Command</th>
+					<th>Environment Variables</th>
+					<th width="50">Processes</th>
+					<th width="50">Process Ping Interval</th>
+					<th width="50">Process Ping Delay</th>
+				</tr>
+			</thead>
 
-            <tbody>
-                <tr>
-                    <td>{{ .ProxyBackend.Domain }}</td>
-                    <td>{{ .ProxyBackend.Path }}</td>
-                    <td><code>{{ .ProxyBackend.Command }}</code></td>
-                    <td><code>{{ .ProxyBackend.Env }}</code></td>
-                    <td>{{ .ProxyBackend.UpNumprocs }}/{{ .ProxyBackend.Numprocs }}</td>
-                    <td>{{ .ProxyBackend.Ping }}</td>
-                    <td>{{ .ProxyBackend.Delay }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+			<tbody>
+				<tr>
+					<td>{{ .ProxyBackend.Domain }}</td>
+					<td>{{ .ProxyBackend.Path }}</td>
+					<td><code>{{ .ProxyBackend.Command }}</code></td>
+					<td><code>{{ .ProxyBackend.Env }}</code></td>
+					<td>{{ .ProxyBackend.UpNumprocs }}/{{ .ProxyBackend.Numprocs }}</td>
+					<td>{{ .ProxyBackend.Ping }}</td>
+					<td>{{ .ProxyBackend.Delay }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
+
+{{ if .ProxyBackend.IsDocker }}
+{{ else }}
+<div class="row">
+	<div class="large-12 columns">
+		<h3>Processes</h3>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-12 columns">
+		<table class="full-width">
+			<thead>
+				<tr>
+					<th>PID</th>
+					<th>Command</th>
+					<th>Environment Variables</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				{{ range $element := .ProxyBackend.Process.Instances }}
+				<tr>
+					<td>{{ $element.ProcessWrapper.Pid }}</td>
+					<td><code>{{ $element.Command }}</code></td>
+					<td><code>{{ $element.Env }}</code></td>
+				</tr>
+				{{ end }}
+			</tbody>
+		</table>
+	</div>
+</div>
+{{ end }}
 `
 }
 
