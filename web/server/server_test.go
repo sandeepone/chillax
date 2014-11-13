8cd5f14070d39d622dab3d7a7b08f63e569392cd
Didip Kerabat <didipk@gmail.com>
2014-11-12 23:13:41 -0800

    Fix server_test

--- web/server/server_test.go
+++ web/server/server_test.go
@@ -8,15 +8,9 @@ import (
 func TestNewServerWithoutConfigPath(t *testing.T) {
 	os.Setenv("CHILLAX_ENV", "test")
 
-	server, err := NewServer()
+	_, err := NewServer()
 
 	if err != nil {
 		t.Errorf("Should be able to create server without CONFIG_PATH. Error: %v", err)
-	}
-	if server.Paths["AdminProxies"] == "" {
-		t.Errorf("server.AdminProxiesPath should not be empty")
-	}
-	if server.Paths["AdminStatic"] == "" {
-		t.Errorf("server.AdminStaticPath should not be empty")
 	}
 }
